bff8bc2708c18c23cfcca9a212b277607de00395
Jordan <jordan.dillard13@gmail.com>
2019-05-10 18:57:49 -0500

    Progress on JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,61 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+//-----------------------------------------------
+  // your code goes here
+
+  //if undefined or function() return null
+
+  // handle object input
+    // ...
+
+
+  // handle unstringafiable value input
+    // ...
+
+ // return stringifyJSON until we reach innermost value
+
+ //if object put key in "" and wrap object in ''
+
+ //if array wrap array in ''
+
+ //if
+ //----------------------------------------------
+
+var resultArr = [];
+
+function arrFunc(obj) {
+for (var x = 0; x < obj.length; x++) {
+  if(typeof(obj[x]) === 'string') {
+   obj[x] = `"${obj[x]}"`
+   continue
+  }
+  return resultArr.push(arrFunc(obj));
+  }
+  return obj;
+}
+
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return `${obj}`
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+  if (Array.isArray(obj) && obj.length === 0) {
+  return `[${obj}]`
+  }
+  if (Array.isArray(obj) && obj.length > 0) {
+    arrFunc(obj);
+    return `"${resultArr}"`
+  }
+  return obj;
 };
+var test = [8]
+
+
+console.log(stringifyJSON(test));
